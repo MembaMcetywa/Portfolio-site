885cbf974514b20112e18f38ac3a270107e5b436
MembaSovTech <membathisi.mcetywa@sovtech.com>
2022-11-28 09:01:34 +0200

    feature: added latest projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,43 +1,64 @@
+import { DiMongodb } from "react-icons/di";
+
 export const projects = [
   {
-    title: 'Facebook UI Clone',
-    description: "Built using Flutter. It is a clone of the Facebook interface. It doesn't yet have any functionality.",
-    tags: ['Flutter', 'Dart'],
-    source: 'https://github.com/MembaMcetywa/facebook_clone',
-    
+    title: "Instill Education",
+    description:
+      "Built using React, React Native, Chakra UI, NativeBase, Prisma, GraphQL, MongoDB. Consists of both a DMP & a mobile app. the project is currently in beta and can be downloaded from the Play Store.",
+    tags: ["React", "TypeScript", "GraphQL", "DiMongodb"],
+    source: "https://instill.education",
+
     id: 0,
   },
   {
-    title: 'Chat UI',
-    description:"Built using Flutter. It is a simple chat app UI. Also views conversations. Very smooth UI.",
-    tags: ['Flutter', 'Dart'],
-    source: 'https://github.com/MembaMcetywa/zima_chat',
+    title: "Rapid Response",
+    description: "Simple reaction speed game built using Vue.Js. ",
+    tags: ["Vue", "JavaScript"],
+    source: "https://github.com/MembaMcetywa/rapid-response",
+
     id: 1,
   },
   {
-    title: 'Azania Hospital',
-    description: "Built using Django. Simple Hospital management system. Focused on doctors but gives patients independence.",
-    tags: ['Django', 'Python'],
-    source: 'https://github.com/MembaMcetywa/azanianhospital',
+    title: "Facebook UI Clone",
+    description:
+      "Built using Flutter. It is a clone of the Facebook interface. It doesn't yet have any functionality.",
+    tags: ["Flutter", "Dart"],
+    source: "https://github.com/MembaMcetywa/facebook_clone",
+
     id: 2,
+  },
+  {
+    title: "Chat UI",
+    description:
+      "Built using Flutter. It is a simple chat app UI. Also views conversations. Very smooth UI.",
+    tags: ["Flutter", "Dart"],
+    source: "https://github.com/MembaMcetywa/zima_chat",
+    id: 3,
+  },
+  {
+    title: "Azania Hospital",
+    description:
+      "Built using Django. Simple Hospital management system. Focused on doctors but gives patients independence.",
+    tags: ["Django", "Python"],
+    source: "https://github.com/MembaMcetywa/azanianhospital",
+    id: 4,
   },
 
   {
-    title: 'Employee Management System',
-    description: "Built using Java. HR employee management system to help ease the burden of admin.",
-    tags: ['Java', 'MySQL'],
-    source: 'https://github.com/MembaMcetywa/Boston-Employee-Management-System',
-    id: 3,
+    title: "Employee Management System",
+    description:
+      "Built using Java. HR employee management system to help ease the burden of admin.",
+    tags: ["Java", "MySQL"],
+    source: "https://github.com/MembaMcetywa/Boston-Employee-Management-System",
+    id: 5,
   },
-
-
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Matric', },
-  { year: 2018, text: 'Intro to programming', },
-  { year: 2019, text: 'Systems development using Java and MySQL', },
-  { year: 2020, text: 'Intro to web development', },
-  { year: 2021, text: 'Systems and web developement private tutor', },
-  { year: 2022, text: 'Fully fledged software developer', },
+  { year: 2018, text: "Intro to programming" },
+  { year: 2019, text: "Systems development using Java and MySQL" },
+  { year: 2020, text: "Intro to web development" },
+  { year: 2021, text: "Systems and web developement private tutor" },
+  { year: 2022, text: "Software developer intern" },
+  { year: 2023, text: "Junior Software developer" },
 ];
